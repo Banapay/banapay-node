6bd604e6b44ee48f49b199da74461df1f532dd2b
Pengwius <tomekftomekf@gmail.com>
2023-12-29 20:24:14 +0100

    Init client with private key

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,11 +4,24 @@ require('dotenv').config();
 
 const mnemonic: string = process.env.MNEMONIC || "";
 const publicKey: string = process.env.PUBLIC_KEY || "";
+const privateKey: string = process.env.PRIVATE_KEY || "";
 
 describe('index.js', () => {
   test('Init client with mnemonic phrase', () => {
     const client = new BanapayClient(mnemonic);
     const clientPublicKey = client.getPublicKey();
     expect(clientPublicKey).toEqual(publicKey);
+  });
+
+  test('Init client with private key', () => {
+    const client = new BanapayClient(undefined, privateKey, undefined);
+    const clientPublicKey = client.getPublicKey();
+    expect(clientPublicKey).toEqual(publicKey);
+  });
+
+  test('Init client without any data', () => {
+    expect(() => {
+      const client = new BanapayClient();
+    }).toThrowError("Mnemonic or private key is required");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,34 @@ import { Keypair } from "@solana/web3.js";
 import nacl from 'tweetnacl';
 import * as bip39  from 'bip39';
 import { derivePath } from 'ed25519-hd-key';
+import * as bs58 from 'bs58';
 
 class BanapayClient {
   private rpcUrl: string;
   private keypair: Keypair;
 
-  constructor(mnemonic: string, rpcUrl?: string) {
+  constructor(mnemonic?: string, privateKey?: string,rpcUrl?: string) {
     this.rpcUrl = rpcUrl || "https://api.mainnet-beta.solana.com";
-    this.keypair = this.createKeypair(mnemonic);
+    if (privateKey) {
+      this.keypair = this.createKeypairFromPrivateKey(privateKey);
+      return;
+    }
+    if (mnemonic) {
+      this.keypair = this.createKeypairFromMnemonic(mnemonic);
+      return;
+    }
+    throw new Error("Mnemonic or private key is required");
   }
 
-  private createKeypair(mnemonic: string): Keypair {
+  private createKeypairFromPrivateKey(privateKey: string): Keypair {
+    const keypair = Keypair.fromSecretKey(
+      bs58.decode(privateKey)
+    );
+    
+    return keypair;
+  }
+
+  private createKeypairFromMnemonic(mnemonic: string): Keypair {
     let seed = bip39.mnemonicToSeedSync(mnemonic);
     const seedBuffer = Buffer.from(seed).toString('hex');
     const path44Change = "m/44'/501'/0'/0'";
