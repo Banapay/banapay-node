9fc6efda10e50404b7ee15e59495fae4906a7b60
Pengwius <tomekftomekf@gmail.com>
2023-12-29 19:24:45 +0100

    Init client with mnemonic phrase

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,14 @@
 import BanapayClient from "./index";
 
+require('dotenv').config();
+
+const mnemonic: string = process.env.MNEMONIC || "";
+const publicKey: string = process.env.PUBLIC_KEY || "";
+
 describe('index.js', () => {
-  test('Banapay client', () => {
-    const client = new BanapayClient("dwadaw");
-    expect(client).toEqual(new BanapayClient("dwadaw"));
+  test('Init client with mnemonic phrase', () => {
+    const client = new BanapayClient(mnemonic);
+    const clientPublicKey = client.getPublicKey();
+    expect(clientPublicKey).toEqual(publicKey);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
+import { Keypair } from "@solana/web3.js";
+import nacl from 'tweetnacl';
+import * as bip39  from 'bip39';
+import { derivePath } from 'ed25519-hd-key';
+
 class BanapayClient {
-  constructor(seed: string) {
-    this.seed = seed;
+  private rpcUrl: string;
+  private keypair: Keypair;
+
+  constructor(mnemonic: string, rpcUrl?: string) {
+    this.rpcUrl = rpcUrl || "https://api.mainnet-beta.solana.com";
+    this.keypair = this.createKeypair(mnemonic);
   }
 
-  public seed: string;
+  private createKeypair(mnemonic: string): Keypair {
+    let seed = bip39.mnemonicToSeedSync(mnemonic);
+    const seedBuffer = Buffer.from(seed).toString('hex');
+    const path44Change = "m/44'/501'/0'/0'";
+    const derivedSeed = derivePath(path44Change, seedBuffer).key;
+    const keypair = Keypair.fromSeed(derivedSeed);
+    return keypair;
+  }
+
+  public getPublicKey(): string {
+    return this.keypair.publicKey.toBase58();
+  }
 }
 
 export default BanapayClient;
